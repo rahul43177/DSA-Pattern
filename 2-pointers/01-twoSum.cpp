563a96d776735b4fefd34bfa2e8b1ba0d2ebec46
Rahul <rahulmishra4317@gmail.com>
2025-11-29 02:54:28 +0530

    main

--- 2-pointers/01-twoSum.cpp
+++ 2-pointers/01-twoSum.cpp
@@ -6,24 +6,24 @@ using namespace std;
 
 vector<int> twoSum(vector<int> arr , int target) {
     int n = arr.size();
-    unordered_map<int , int> mpp; 
-    for(int i = 0;i<n;i+=1) {
+    unordered_map<int , int> mpp;
+    for(int i = 0;i<n;i++) {
         int currentValue = arr[i];
-        int compliment = target-currentValue; 
+        int compliment = target - currentValue;
         if(mpp.find(compliment)!=mpp.end()) {
-            return {mpp[compliment] , i}; 
+            return {mpp[compliment] , i};
         }
         mpp[currentValue] = i; 
     }
-    return {-1, -1};
+    return { -1 , -1};
 }
 
 int main() {
-    vector<int> arr = {10 , 14, 5 ,8 ,2 ,19};
-    int target = 16; 
-    
-    vector<int> pairSum = twoSum(arr , target);
-    for(auto val : pairSum) {
-        cout << val << " " ;
+    vector<int> arr = {3,3};
+    int target = 6;
+
+    vector<int> pairs = twoSum(arr, target);
+    for(auto val : pairs) {
+        cout << val << " ";
     }
 }
